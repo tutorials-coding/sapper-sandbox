b63cba04bdd3cad299043ae3dc70059a6f0c7b73
Vadim <korobeinikovv@mail.ru>
2021-04-02 12:45:11 +0300

    Show articles list

--- src/routes/articles/index.svelte
+++ src/routes/articles/index.svelte
@@ -1,5 +1,26 @@
+<script context="module">
+  export function preload() {
+    return this.fetch(`articles.json`)
+      .then(r => r.json()).then(articles => {
+        return {
+          articles
+        };
+      });
+  }
+</script>
+
+<script>
+  export let articles;
+</script>
+
 <svelte:head>
-	<title>Articles</title>
+  <title>Articles</title>
 </svelte:head>
 
 <h1>Articles</h1>
+
+<ul>
+  {#each articles as article}
+    <li><a rel="prefetch" href="articles/{article.id}">{article.title}</a></li>
+  {/each}
+</ul>
