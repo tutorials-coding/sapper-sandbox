64ac423ec42bfea6166d858859965856884bcbd7
Vadim <korobeinikovv@mail.ru>
2021-04-02 15:21:48 +0300

    Add notes, dynamic routes

---
+++ src/routes/notes/[...slug].svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  export async function preload({ params }) {
+    const [ year, month, day, id ] = params.slug
+    const res = await this.fetch(`articles/${year}/${month}/${day}/${id}.json`);
+    const data = await res.json();
+
+    if (res.status === 200) {
+      return { article: data };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
+</script>
+
+<script>
+  export let article;
+</script>
+
+<svelte:head>
+  <title>{article.title}</title>
+</svelte:head>
+
+<h1>{article.title}</h1>
